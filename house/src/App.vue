ffe4552475a0fae1ee1b9a8e1a0387990d73c5a1
yoo <edel1212@naver.com>
2023-05-19 17:42:37 +0900

    v-if 문

--- house/src/App.vue
+++ house/src/App.vue
@@ -1,27 +1,26 @@
 <template>
 
   <div class="menue">
-    <!-- <a v-for="item in 3" :key="item" href="">home</a> -->
     <a v-for="(item,index) in topMenu" :key="index" href="">
         {{item}}[{{index}}]
     </a>
   </div>
 
-  <div>
-    <!-- <h4 :id="idTest" :style="styleTest">xxx 원룸</h4> -->
-    <h4>xxx 원룸</h4>
-    <!-- <p>{{ price1 }} 만원</p> -->
-    <p>10 만원</p>
-  </div>
-
-  <div class="itemSection" v-for="(item, index) in products" :key="index">
+  <div class="itemSection" v-for="(item, index) in products" :key="index">    
+    <img :src="require(`@/assets/img/room${index}.jpg`)" class="room-img">       
     <h4>{{item.name}}</h4>
     <p>{{(index+1) *10}} 만원</p>  
-    <!-- <button @click="item.reportCnt++">허위매물 신고</button> <span>신고수 : {{reportCnt}}</span> -->
     <button @click="increase(index)">허위매물 신고</button> <span>신고수 : {{item.reportCnt}}</span>
-    <!-- <button @mouseover="reportCnt++">허위매물 신고</button> <span>신고수 : {{reportCnt}}</span> -->
   </div>
   
+  <!-- Modal -->
+  <div class="black-bg" v-if="modalState">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지 내용</p>
+      <button @click="this.modalState = false">닫기</button>
+    </div>
+  </div>
 
 </template>
 
@@ -34,7 +33,8 @@ export default {
       topMenu : ["Home", "Shop", "About"],
       products : [ {name:"역삼동원룸", reportCnt : 0 }
                  , {name:"천호동원룸", reportCnt : 0 }
-                  , {name:"마보구원룸", reportCnt : 0 } ]      
+                  , {name:"마보구원룸", reportCnt : 0} ],
+      modalState : true,                  
     }
   },
   methods:{   // 함수를 담는곳
@@ -55,6 +55,9 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
+.body{
+  margin: 0px;
+}
 .menue{
   background: darkslateblue;
   padding:15px;
@@ -67,4 +70,22 @@ export default {
 .itemSection{
   border-bottom: 1px solid black;
 }
+.room-img{
+  /* width: 100%; */
+  margin-top: 40px;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%; height:100%;
+  background: rgba(0,0,0,0.5);
+  position: fixed; padding: 20px;
+  top : 1px;
+}
+.white-bg {
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+} 
 </style>
