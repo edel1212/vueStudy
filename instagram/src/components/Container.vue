bac4900b3c76fe903e1501542bab7723dd14b87a
yoo <edel1212@naver.com>
2023-05-24 15:22:29 +0900

    add Component

---
+++ instagram/src/components/Container.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-for="(item,idx) in instaDataArr" :key="idx">
+    <Post v-bind:instaData="instaDataArr[idx]" />
+  </div>
+</template>
+
+<script>
+import Post from "/src/components/Post.vue"
+export default {
+    name : "ContainerComponent"
+    ,components :{
+        Post,
+    },
+    props : {
+        instaDataArr : Array
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ instagram/src/components/Post.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="post">    
+    <div class="post-header">
+      <div class="profile" :style="{'background-image' : `url(${instaData.userImage})`}"></div>
+      <span class="profile-name">{{instaData.name}}</span>
+    </div>
+    <div class="post-body"  :style="{'background-image' : `url('${instaData.postImage}')`}"></div>
+    <div class="post-content">
+      <p>{{instaData.likes}} Likes</p>
+      <p><strong>{{instaData.filter}}</strong> {{instaData.content}}</p>
+      <p class="date">{{instaData.date}}</p>
+    </div>
+</div> 
+</template>
+
+<script>
+export default {
+    name : "PostComponent",
+    props : {
+        instaData : Object
+    }
+}
+</script>
+
+<style>
+    .post {
+    width: 100%;
+    }
+    .profile {
+    background-image: url("https://placeimg.com/100/100/arch");
+    width: 30px;
+    height: 30px;
+    background-size: 100%;
+    border-radius: 50%;
+    float: left;
+    }
+    .profile-name {
+    display: block;
+    float: left;
+    padding-left: 10px;
+    padding-top: 7px;
+    font-size: 14px;
+    }
+    .post-header {
+    height: 30px;
+    padding: 10px;
+    }
+    .post-body {
+    background-image: url("https://placeimg.com/640/480/animals");
+    height: 450px;
+    background-position: center;
+    background-size: cover;
+    }
+    .post-content {
+    padding-left: 15px;
+    padding-right: 15px;
+    font-size: 14px;
+    }
+    .date {
+    font-size: 11px;
+    color: grey;
+    margin-top: -8px;
+    } 
+</style>
