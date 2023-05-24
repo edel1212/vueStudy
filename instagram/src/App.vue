dcbbb79d6717d12242524f57a0900519c78c0e4c
yoo <edel1212@naver.com>
2023-05-24 22:26:44 +0900

    글 작성 기능 추가

--- instagram/src/App.vue
+++ instagram/src/App.vue
@@ -4,19 +4,39 @@
       <li>Cancel</li>
     </ul>
     <ul class="header-button-right">
-      <li>Next</li>
+      <li v-if="tapStep === 1" @click="tapStep++">Next</li>
+      <li v-if="tapStep === 2" @click="publish">Register</li>
     </ul>
     <img src="./assets/logo.png" class="logo" />
   </div>
 
-  <Container v-bind:instaDataArr="instaDataArr"/>
+  <!-- 
+    Comonent를 사용해도 되나 Router를 사용해야하는이유
+    - 👉 뒤로가기 버튼 떄문이다. [중요!]
+   -->
+  <Container v-bind:instaDataArr="instaDataArr" v-bind:tapStep="tapStep"
+   v-bind:updateImgURL="updateImgURL" @write="content = $event"/>
 
+
+  <!-- 더보기 버튼 -->
+  <button @click="more">더보기</button>
+
+  <!-- Footer -->
   <div class="footer">
     <ul class="footer-button-plus">
-      <input type="file" id="file" class="inputfile" />
+      <input @change="imagUpload" accept="image/*" type="file" id="file" class="inputfile" />
       <label for="file" class="input-plus">+</label>
     </ul>
  </div>
+
+<div @click="tapClick">
+  <button data-idx="0">내용0</button>
+  <button data-idx="1">내용1</button>
+  <button data-idx="2">내용2</button>
+</div>
+<div sytle="margin-top:500px"></div>
+
+
 </template>
 
 <script>
@@ -27,18 +47,82 @@ import instaDataArr from "./assets/dummyData/dummyData"
 // Component import
 import Container from "./components/Container.vue";
 
-
+// Axios import
+import Axios from "axios";
 
 export default {
   name: 'App',
   data (){
     return {
-      instaDataArr 
+      instaDataArr,
+      btnCnt : 0,
+      tapStep : 0,
+      updateImgURL : '',
+      content : ''
     }
   },
   components: {
     Container
   },
+  methods : {
+    /** 더 보기 */
+    more(){
+      // Btn Count에 따른 URL 변화
+      let parBtnCnt = this.btnCnt%2;
+      this.btnCnt ++;
+
+      /**
+       * Axios에서는 Data을 가져올 경우 .data를 통해 JSON으로 반환 받는다.
+       * - fetch 경우 .json()임 차이가 있음 잊지말자
+       */
+      Axios.get(`https://codingapple1.github.io/vue/more${parBtnCnt}.json`)
+      .then((res)=>res.data)
+      .then((result)=>{        
+        // Vue는 실시간 재 랜더링을 해주므로 
+        // data에 추가만 해주면 된다.
+        this.instaDataArr.push(result);
+      }).catch(err=>{
+        console.log(err);
+      });
+      
+    },
+    /** 탭버튼 Event */
+    tapClick(e){      
+      if(e.target.nodeName !== 'BUTTON') return;
+      this.tapStep = e.target.dataset.idx;
+    },
+    /** 파일 업로드 */
+    imagUpload(e){
+      const files = e.target.files;
+      if(files.length === 0) return;
+
+      // 확장자 체크
+      for(let file of files){
+        if(file.type !== 'image/jpeg') return;        
+        this.updateImgURL = URL.createObjectURL(file);
+      } 
+      // 등록 스탭으로 변경
+      this.tapStep = 1;
+    },
+    /** 글 작성 */
+    publish(){
+      const registerData = {
+        name: "Yoo jh",
+        userImage: "https://placeimg.com/100/100/arch",
+        postImage: this.updateImgURL,
+        likes: 0,
+        date: new Date(),
+        liked: false,
+        content: this.content,
+        filter: "perpetua"
+      };
+      // 👉 unshift() << 왼쪽에 데이터를 추가
+      this.instaDataArr.unshift(registerData);
+      // 스탭 초기화
+      this.tapStep = 0;
+    }
+
+  }
 }
 </script>
 
--- instagram/src/components/Container.vue
+++ instagram/src/components/Container.vue
@@ -1,22 +1,98 @@
 <template>
-  <div v-for="(item,idx) in instaDataArr" :key="idx">
-    <Post v-bind:instaData="instaDataArr[idx]" />
+  <div v-if="tapStep == 0">
+    <div v-for="(item,idx) in instaDataArr" :key="idx">
+        <Post v-bind:instaData="instaDataArr[idx]" />
+    </div>
+  </div>  
+
+  <!-- 필터선택페이지 -->
+  <div v-if="tapStep == 1">
+    
+    <div class="upload-image" :style="`background-image : url(${updateImgURL})`"></div>
+    <div class="filters">
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+        <div class="filter-1"></div>
+    </div>  
   </div>
+  
+
+  <!-- 글작성페이지 -->
+  <div v-if="tapStep == 2">
+    <div class="upload-image"  :style="`background-image : url(${updateImgURL})`"></div>
+    <div class="write">
+        <textarea @input="$emit('write', $event.target.value)" class="write-box">write!</textarea>
+    </div>
+  </div>
+  
 </template>
 
 <script>
 import Post from "/src/components/Post.vue"
 export default {
+    data(){
+        return {
+        }
+    },
     name : "ContainerComponent"
     ,components :{
         Post,
     },
     props : {
-        instaDataArr : Array
+        instaDataArr : Array,
+        tapStep : Number,
+        updateImgURL : String
+    },
+    methods :{
+                
     }
+    
 }
 </script>
 
 <style>
-
+    .upload-image{
+    width: 100%;
+    height: 450px;
+    background: cornflowerblue;
+    background-size : cover;
+    }
+    .filters{
+    overflow-x:scroll;
+    white-space: nowrap;
+    }
+    .filter-1 {
+    width: 100px;
+    height: 100px;
+    background-color: cornflowerblue;
+    margin: 10px 10px 10px auto;
+    padding: 8px;
+    display: inline-block;
+    color : white;
+    background-size: cover;
+    }
+    .filters::-webkit-scrollbar {
+    height: 5px;
+    }
+    .filters::-webkit-scrollbar-track {
+    background: #f1f1f1; 
+    }
+    .filters::-webkit-scrollbar-thumb {
+    background: #888; 
+    border-radius: 5px;
+    }
+    .filters::-webkit-scrollbar-thumb:hover {
+    background: #555; 
+    }
+    .write-box {
+    border: none;
+    width: 90%;
+    height: 100px;
+    padding: 15px;
+    margin: auto;
+    display: block;
+    outline: none;
+    }
 </style>
