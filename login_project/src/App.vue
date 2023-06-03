225bf849dc3e37b2a9e504e82b69d7efb8d0226a
yoo <edel1212@naver.com>
2023-06-03 23:06:42 +0900

    add Axios

--- login_project/src/App.vue
+++ login_project/src/App.vue
@@ -1,18 +1,42 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
 
+  <br/>
+
+  <label for="idInput">ID :</label>
+  <input type="text" id="idInput" name="id">
+  
+  <br/>
+  <label for="pwInput">PW :</label>
+  <input type="password" id="pwInput" name="pw">
+  <button @click="login(event)">login</button>
+
   <p>-----------------------</p>
   <router-view></router-view>
   <p>-----------------------</p>
 </template>
 
 <script>
-
 export default {
   name: 'App',
   components: {
   }
 }
+</script>
+
+<script setup>
+
+/**
+ * login Event
+ */
+const login = (e)=>{
+  const id = document.querySelector("#idInput").value;
+  const pw = document.querySelector("#pwInput").value;
+  console.log(id);
+  console.log(pw);
+}
+
+
 </script>
 
 <style>
